39d1a5cde8f184cb3ffebfb1b323ac7bc4644bc5
anshul.agarwal <anshul.agarwal@sky.uk>
2016-04-05 11:33:49 +0530

    New Feature added

--- MultiThreadingExamples/MultiThreadingExamples/JoinExample.cs
+++ MultiThreadingExamples/MultiThreadingExamples/JoinExample.cs
@@ -17,7 +17,7 @@ namespace MultiThreadingExamples
             //t.Abort();
             Console.WriteLine("Thread t has ended!");
         }
-
+        //New Feature Added
         static void Go()
         {
             for (int i = 0; i < 1000; i++) Console.Write("y");
